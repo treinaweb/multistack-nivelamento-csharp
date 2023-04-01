174db105b2d86df4b3845324599402fda5d5066f
Cleyson Lima <cleysonph@gmail.com>
2023-04-01 02:53:04 -0300

    02:07 - Estruturas condicionais: if/else

---
+++ Aula06/Program.cs
@@ -0,0 +1,14 @@
+﻿var idade = 15;
+if (idade >= 18)
+{
+    Console.WriteLine("Você é maior de idade!");
+}
+else if (idade >= 16)
+{
+    Console.WriteLine("Você é menor de idade, mas já pode votar!");
+}
+else
+{
+    Console.WriteLine("Você é menor de idade!");
+}
+Console.WriteLine("Terminou o programa");
