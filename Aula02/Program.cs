70a13ab6aef08c16534e3130041e947a384214ef
Cleyson Lima <cleysonph@gmail.com>
2023-04-01 00:19:22 -0300

    02:03 - Variáveis, constantes e tipos de dados

---
+++ Aula02/Program.cs
@@ -0,0 +1,22 @@
+﻿string nome = "Cleyson";
+int idade = -20;
+uint idade2 = 20;
+float altura = 1.7F;
+double altura2 = 1.7;
+decimal altura3 = 1.7M;
+bool ehProgramador = true;
+
+Console.WriteLine(nome);
+Console.WriteLine(idade);
+Console.WriteLine(idade2);
+Console.WriteLine(altura);
+Console.WriteLine(altura2);
+Console.WriteLine(altura3);
+Console.WriteLine(ehProgramador);
+
+var variavel = "alguma coisa";
+
+Console.WriteLine(variavel);
+
+const int MaximoDeAlunos = 100;
+Console.WriteLine(MaximoDeAlunos);
