166338333d2e9e73da18a5eae8d3514eaff9f815
Cleyson Lima <cleysonph@gmail.com>
2023-04-01 01:20:20 -0300

    02:05 - Operadores relacionais

---
+++ Aula04/Program.cs
@@ -0,0 +1,17 @@
+﻿// Maior que
+Console.WriteLine(15 > 10);
+
+// Menor que
+Console.WriteLine(10 < 15);
+
+// Igualdade
+Console.WriteLine(10 == 11);
+
+// Diferença
+Console.WriteLine(10 != 10);
+
+// Maior ou igual
+Console.WriteLine(5 >= 10);
+
+// Menor ou igual
+Console.WriteLine(15 <= 10);
