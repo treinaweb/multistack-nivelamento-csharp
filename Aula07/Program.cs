de36005523bf839f696988383b6182678087e121
Cleyson Lima <cleysonph@gmail.com>
2023-04-01 03:05:58 -0300

    02:08 - Estruturas condicionais: switch/case

---
+++ Aula07/Program.cs
@@ -0,0 +1,16 @@
+﻿var mesDoAno = 4;
+switch (mesDoAno)
+{
+    case 1:
+        Console.WriteLine("Janeiro");
+        break;
+    case 2:
+        Console.WriteLine("Fevereiro");
+        break;
+    case 3:
+        Console.WriteLine("Março");
+        break;
+    default:
+        Console.WriteLine("Mês inválido");
+        break;
+}
