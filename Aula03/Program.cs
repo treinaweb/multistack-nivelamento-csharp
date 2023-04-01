2829df805f5a72b379455950576ee1d21521da79
Cleyson Lima <cleysonph@gmail.com>
2023-04-01 00:40:01 -0300

    02:04 - Operadores matemáticos

---
+++ Aula03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Soma
+var soma = 10 + 10;
+Console.WriteLine(soma);
+
+// Subtração
+var subtracao = 10 - 10;
+Console.WriteLine(subtracao);
+
+// Multiplicação
+var multiplicacao = 10 * 10;
+Console.WriteLine(multiplicacao);
+
+// Divisão
+var divisao = 5 / 2.0;
+Console.WriteLine(divisao);
+
+// Modulo
+var modulo = 5 % 2;
+Console.WriteLine(modulo);
