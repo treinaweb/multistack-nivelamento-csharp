c53430b79bda2e13341901c3dbc95797e845517a
Cleyson Lima <cleysonph@gmail.com>
2023-04-02 00:06:49 -0300

    02:09 - Estruturas de repetição: while e do-while

---
+++ Aula08/Program.cs
@@ -0,0 +1,14 @@
+﻿var contadora = 101;
+while (contadora <= 100)
+{
+    Console.WriteLine("A váriavel contadora vale: " + contadora);
+    contadora++;
+}
+Console.WriteLine("Terminou o while");
+
+do
+{
+    Console.WriteLine("A váriavel contadora vale: " + contadora);
+    contadora++;
+} while (contadora <= 100);
+Console.WriteLine("Terminou o do while");
