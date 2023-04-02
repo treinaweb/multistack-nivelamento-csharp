2b8c92fc3da6bf488282b101c95693883de15ce6
Cleyson Lima <cleysonph@gmail.com>
2023-04-02 02:20:41 -0300

    02:11 - Collections: Listas

---
+++ Aula10/Program.cs
@@ -0,0 +1,21 @@
+﻿// var notas = new List<double>();
+// notas.Add(10.0);
+// notas.Add(8.7);
+// notas.Add(9.2);
+// Console.WriteLine(notas[0]);
+// Console.WriteLine(notas[1]);
+// Console.WriteLine(notas[2]);
+
+// notas[0] = 7.2;
+// Console.WriteLine(notas[0]);
+
+// // notas.Remove(9.2);
+// // Console.WriteLine(notas[2]);
+
+// notas.RemoveAt(1);
+// Console.WriteLine(notas[1]);
+
+var notas = new List<double>() { 10.0, 8.7, 9.1 };
+Console.WriteLine(notas[0]);
+Console.WriteLine(notas[1]);
+Console.WriteLine(notas[2]);
