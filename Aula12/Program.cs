e0e0aeb42dc86517d66a8cf9e29818f1c9f4bd3f
Cleyson Lima <cleysonph@gmail.com>
2023-04-02 02:40:18 -0300

    02:13 - Estruturas de repetição foreach

---
+++ Aula12/Program.cs
@@ -0,0 +1,24 @@
+﻿var alunos = new List<string>() { "Cleyson", "Elton", "Ariel", "Ana" };
+
+// for (var index = 0; index < alunos.Count; index++)
+// {
+//     Console.WriteLine(alunos[index]);
+// }
+
+foreach (var aluno in alunos)
+{
+    Console.WriteLine(aluno);
+}
+
+var medias = new Dictionary<string, double>()
+{
+    { "Elton", 10.0 },
+    { "Cleyson", 8.7 },
+    { "Ariel", 7.7 },
+    { "Ana", 9.1 }
+};
+
+foreach (var aluno in medias)
+{
+    Console.WriteLine($"{aluno.Key} - {aluno.Value}");
+}
