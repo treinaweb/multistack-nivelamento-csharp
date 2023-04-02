c29e50f51c35e7b71a5c8371a468b676e49657ca
Cleyson Lima <cleysonph@gmail.com>
2023-04-02 02:30:36 -0300

    02:12 - Collections: Dicionários

---
+++ Aula11/Program.cs
@@ -0,0 +1,17 @@
+﻿var medias = new Dictionary<string, double>();
+medias.Add("Elton", 10.0);
+medias.Add("Cleyson", 9.5);
+medias.Add("Amauri", 7.2);
+
+Console.WriteLine(medias["Elton"]);
+Console.WriteLine(medias["Cleyson"]);
+Console.WriteLine(medias["Amauri"]);
+
+medias["Cleyson"] = 10.0;
+Console.WriteLine(medias["Cleyson"]);
+
+medias["Ana"] = 9.1;
+Console.WriteLine(medias["Ana"]);
+
+medias.Remove("Cleyson1");
+Console.WriteLine(medias["Cleyson"]);
